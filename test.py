664e50262a94a3c94c157d16ceb0260d717aaa16
NIkola Vasović <72806243+vaskemeteo@users.noreply.github.com>
2021-05-31 15:34:55 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,8 @@ neki_string = "Monak Djokovic"
 
 for i in range(len(neki_string)):
     print(neki_string[i])
+
+
+    
+    
+# neko je ovde predlozio izmene, povlacimo projekat na nasu masinu radi sinhronizacije
